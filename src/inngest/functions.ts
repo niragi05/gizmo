1691e5a12a79ff3e31a00c4a2b8c9549f158ab58
niragi05 <niragimasalia@gmail.com>
2026-01-10 17:00:19 -0700

    firecrawl for scraping urls

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,38 @@
+import { firecrawl } from "@/lib/firecrawl";
 import { inngest } from "./client";
+
+const URL_REGEX = /https?:\/\/[^\s]+/g;
 
 export const helloWorld = inngest.createFunction(
     {id: 'hello-world'},
     {event: 'test/hello.world'},
     async ({event, step}) => {
-        await step.sleep('wait-a-moment', "1s");
-        return { message: `Hello, ${event.data.email}!` }
+        const { prompt } = event.data as { prompt: string };
+
+        const urls = await step.run('extract-urls', async () => {
+            return prompt.match(URL_REGEX) || [];
+        }) as string[];
+
+        const scrapedContent = await step.run('scrape-urls-content', async () => {
+            const results = await Promise.all(
+                urls.map(async (url) => {
+                    const result = await firecrawl.scrape(
+                        url,
+                        {
+                            formats: ['markdown'],
+                        }
+                    );
+                    return result.markdown ?? null;
+                })
+            )
+
+            return results.filter(Boolean).join("\n\n");
+        });
+
+        const finalPrompt = scrapedContent 
+            ? `Context:\n${scrapedContent}\n\nQuestion: ${prompt}`
+            : prompt;
+        
+        
     }
 )
